816a37614cad88e50c1335c5acd67f880e9c876d
Mikaela 🌸 Wade <mikaelawade@Mikaelas-Air.lan>
2020-05-13 21:41:52 -0400

    05/13/20

--- script.js
+++ script.js
@@ -5,33 +5,42 @@ container.setAttribute('class', 'container');
 
 app.appendChild(container);
 
-var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-request.onload = function () {
 
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then(response => {
+    if (response.status !== 200) {
+      console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+    } else {
+      
+      return response.json();
+    }
+  })
+  .then(data => {
     data.forEach(movie => {
+      // Create a div with a card class
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
+      // Create an h1 and set the text content to the film's title
       const h1 = document.createElement('h1');
       h1.textContent = movie.title;
 
+      const h4 = document.createElement('h4');
+      h4.textContent = movie.release_date;
+
+      // Create a p and set the text content to the film's description
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      movie.description = movie.description.substring(0, 500) // Limit to 500 chars
+      p.textContent = `${movie.description}` //
 
+      // Append the cards to the container element
       container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
-}
 
-request.send();
+      // Each card will contain an h1 and a p
+      card.appendChild(h1);
+      card.appendChild(h4);
+      card.appendChild(p);
+      
+    })
+  })
+
